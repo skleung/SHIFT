99e37aa5afe5155fae02d0f1244eea7083fa4238
Sherman Leung <shermankleung@gmail.com>
2016-08-21 01:07:58 -0700

    fixed detail display bug and added tagline

--- cs96si/js/main.js
+++ cs96si/js/main.js
@@ -7,6 +7,11 @@ var $inputMessage = $('.inputMessage'); // Input message input box
 var $splashPage = $('.splash.page'); // The login page
 var $detailPage = $('.detail.page'); // The chatroom page
 
+$window.load(function() {
+    $detailPage.hide();
+    $("#subtitle-text").width($("#title-text").width() + 28)
+});
+
 $('#learn-more').click(function() {
   $splashPage.fadeOut();
   $detailPage.show();
